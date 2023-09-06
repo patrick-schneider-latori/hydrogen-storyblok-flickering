06a1d3eed50796a15ddf052cb8f67624a393a674
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2023-09-06 20:16:38 +0530

    Fix the flickering in storyblok

--- app/routes/$.jsx
+++ app/routes/$.jsx
@@ -5,43 +5,29 @@ import {isStoryBlokPreview} from '~/lib/utils';
 import {fetchStoryOnRoute} from '~/lib/storyblok';
 
 export async function loader({request, params, context}) {
-  let story;
-
   try {
     const data = await fetchStoryOnRoute({request, params, context});
-
     if (data.story) {
-      story = data.story;
-    }
-
-    if (story) {
       return json({
-        story,
+        story: data.story,
         isStoryBlokPreview: isStoryBlokPreview(request),
       });
     }
   } catch (error) {
-    story = null;
+    throw new Response(`${new URL(request.url).pathname} not found`, {
+      status: 404,
+    });
   }
-
-
-  throw new Response(`${new URL(request.url).pathname} not found`, {
-    status: 404,
-  });
 }
 
 export default function Page() {
   const data = useLoaderData();
-  data.story = useStoryblokState(data?.story);
-  if (data?.story) {
-    return (
-      <StoryblokComponent
-        story={data.story}
-        blok={data.story.content}
-        preview={data.isStoryBlokPreview}
-      />
-    );
-  } else {
-    return <></>;
-  }
+  let story = useStoryblokState(data.story);
+  return (
+    <StoryblokComponent
+      story={story}
+      blok={story.content}
+      preview={data.isStoryBlokPreview}
+    />
+  );
 }
